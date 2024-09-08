d7e8bb0e956a5b7494c8ffa53f5571283bd6f391
shahidx0x <dev.imshahid@gmail.com>
2024-09-08 23:52:44 +0600

    update next.config.js and vercel.json

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,5 @@
 //@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require("@nx/next");
+const { withNx } = require("@nx/next/plugins/with-nx");
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -12,11 +10,7 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  output: "standalone",
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = withNx(nextConfig);
